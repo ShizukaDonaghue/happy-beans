1ca5c2f5d2965a9019cdc80a0bd792f9bb5c3516
Shizuka Donaghue <sdonaghue@gmail.com>
2023-03-10 19:28:12 +0000

    Create RecipeList view in views.py and html templates and style.css

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+    """
+    Displays a list of 9 recipes per page
+    Recipes are ordered by their creation dates in descending order
+    """
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 9
