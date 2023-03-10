f6cdc69e78c10a1163cb769ad6993adb9bfb6896
Shizuka Donaghue <sdonaghue@gmail.com>
2023-03-10 07:10:48 +0000

    Create admin user, add models to admin panel, install summernote

--- recipeapp/admin.py
+++ recipeapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipe, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('ingredients', 'method')
