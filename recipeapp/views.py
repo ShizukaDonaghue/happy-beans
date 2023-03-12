b212b2702cbfe48a93a18cf261d4c953e1f36dfc
Shizuka Donaghue <sdonaghue@gmail.com>
2023-03-12 18:12:49 +0000

    Create the view code for RecipeDetail

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -12,3 +12,27 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 9
+
+
+class RecipeDetail(View):
+    """
+    Displays the detail of a recipe and comments
+    Includes comment form if the user is logged in
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by("created_on")
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked,
+            },
+        )
