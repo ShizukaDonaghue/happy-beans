23126e1d383e8a67ea3aea9d951d59df8f6d8fe9
Shizuka Donaghue <sdonaghue@gmail.com>
2023-03-31 05:23:34 +0000

    Change debug to false to load statics

--- happybeans/settings.py
+++ happybeans/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT')
+DEBUG = False
 
 ALLOWED_HOSTS = ['happy-beans.herokuapp.com', 'localhost']
 
@@ -186,7 +186,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
