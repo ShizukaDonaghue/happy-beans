b37b519ac0849d56ab0ba01741558e9e3fcb1965
Shizuka Donaghue <sdonaghue@gmail.com>
2023-04-04 08:17:06 +0000

    Update script to close the hamburger menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,15 @@
 // Script to close alert messages
 $(document).ready(function() {
-    setTimeout(function(){
+    setTimeout(function() {
         if ($('#msg').length > 0) {
             $('#msg').remove();
         }
     }, 3000);
 });
 
-// Scrip to close Bootstrap hamburger menu when clicked outside the menu
-// Code source: https://stackoverflow.com/questions/71915612/how-to-close-hamburger-menu-when-clicked-outside-the-menu-using-javascript
-$(document).on('click', function () {
-    $('#navbarContents').collapse('hide');
+// Script to close Bootstrap hamburger menu when clicked outside the menu
+$(document).ready(function() {
+    $(document).click(function() {
+        $('#navbarContents').collapse('hide');
+    });
 });
