8f37dc42432c3d993d8d90cee798b5deb30625b0
Shizuka Donaghue <sdonaghue@gmail.com>
2023-04-05 19:28:45 +0000

    Add # noqa to STATICFILES_STORAGE to remove line too long error

--- happybeans/settings.py
+++ happybeans/settings.py
@@ -181,7 +181,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'  # noqa
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
