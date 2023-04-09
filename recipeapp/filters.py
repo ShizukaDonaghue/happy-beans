f983f4a904211d29fdbdb5500f5d828f2902b2f6
Shizuka Donaghue <sdonaghue@gmail.com>
2023-04-09 05:17:39 +0000

    Add comment to recipe filters

--- recipeapp/filters.py
+++ recipeapp/filters.py
@@ -3,6 +3,9 @@ from .models import Recipe
 
 
 class RecipeFilter(django_filters.FilterSet):
+    """
+    Recipe filters used in Browse Recipes page
+    """
 
     class Meta:
         model = Recipe
